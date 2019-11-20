d098ab3197e2bb919e000657f3b4a1d3ef06816f
Muzi Gumede <Muzigumede@outlook.com>
2019-11-20 15:41:49 +0200

    minor

--- fortrait.js
+++ fortrait.js
@@ -7,6 +7,28 @@ function css() {
     ratioPanel = document.createElement("div");
     ratioPanel.style.width = "100px";
     ratioPanel.style.paddingTop = "100%";
-    ratioPanel.innerHTML = "I'm a div."
+    ratioPanel.innerHTML = "I'm a div.";
+    ratioPanel.id = "ratioDiv"
     document.getElementById("panel").append(ratioPanel);
+
+    myAssetsBulk = document.getElementsByClassName("pictures");
+    console.log(myAssetsBulk)
+
+    // myAssetsCloneClass = myAssetsBulk.cloneNode(true);
+
+
+
+    //document.body.appendChild(myAssetsClone);
+
+    assetsLength = myAssetsBulk.length
+    console.log(assetsLength)
+    var picElements = [];
+    for (i = 0; i < assetsLength; i++) {
+        // document.body.append(myAssetsBulk.item(i))
+        picElements[i] = myAssetsBulk.item(i);
+        //document.getElementById("ratioDiv").append(picElements[i])
+        console.log(picElements[i])
+    };
+    document.getElementById("panel").removeChild(myAssetsBulk[0])
+    document.getElementById("panel").removeChild(myAssetsBulk[1])
 }
