4e5220a5a07c6b4718513e10c07164e46c13cd54
Muzi Gumede <Muzigumede@outlook.com>
2019-11-21 09:42:11 +0200

    minor

--- fortrait.js
+++ fortrait.js
@@ -35,11 +35,56 @@ function css() {
             console.log(picElements[1])
         }
         console.log(picElements[i])
-        swap(picElements);
+        myAssetsBulk[0].style.display = "block";
+        myAssetsBulk[1].style.display = "none";
+        myAssetsBulk[2].style.display = "none";
+
     };
 }
 
 function swap(x) {
     // x[0].style.display = "none";
     // x[1].style.display = "none";
+}
+
+function fadeOut(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+function fadeIn(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+counter = 0
+function composite() {
+    myAssetsBulk = document.getElementsByClassName("pictures");
+    if (myAssetsBulk[0].style.display == "block") {
+        fadeOut(myAssetsBulk[0]);
+        fadeIn(myAssetsBulk[1]);
+    } else if (myAssetsBulk[1].style.display == "block") {
+        fadeOut(myAssetsBulk[1]);
+        fadeIn(myAssetsBulk[2]);
+    } else if (myAssetsBulk[2].style.display == "block") {
+        fadeOut(myAssetsBulk[2]);
+        fadeIn(myAssetsBulk[0]);
+    }
+    //     fadeOut(myAssetsBulk[2])
+    //     fadeIn(myAssetsBulk[1])
 }
